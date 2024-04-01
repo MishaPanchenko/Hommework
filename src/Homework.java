073d4695ea83532b2809277e717cffccd2e95a6c
MishaPanchenko <hsakata@mail.ru>
2024-04-01 23:57:42 +0300

    Update Homework.java

--- src/Homework.java
+++ src/Homework.java
@@ -1,125 +1,65 @@
 public class Homework {
-    static int a;
-    static int b;
-    static String str;
-    static int num;
-
-    public static void main(String[] args) {
-
-
-    }
 
     public static void printThreeWords() {
         System.out.println("Задание 1:\nOrange\nBanana\nApple\n");
-
     }
 
-
-    public static void checkSumSign() {
-        int a = -1;
-        int b = 0;
+    public static void checkSumSign(int a, int b) {
+        System.out.print("Задание 2: ");
         int sum = a + b;
-        if (sum >= 0) {
-            System.out.println("Задание 2: Сумма положительная");
-        } else {
-            System.out.println("Задание 2: Сумма отрицательная\n");
-        }
+        String result = sum >= 0 ? "Сумма положительная" : "Сумма отрицательная";
+        System.out.println(result + "\n");
     }
 
-    public static void printColor() {
+    public static void printColor(int value) {
         System.out.print("Задание 3: ");
-        int value = 1434;
         if (value <= 0) {
             System.out.println("Красный\n");
-        } else if (value <= 100 && value > 0) {
+        } else if (value <= 100) {
             System.out.println("Жёлтый\n");
         } else {
             System.out.println("Зелёный\n");
         }
     }
 
-    public static void compareNumbers() {
+    public static void compareNumbers(int a, int b) {
         System.out.print("Задание 4: ");
-        int a = 48;
-        int b = 12;
-        if (a >= b) {
-            System.out.println("a >= b\n");
-        } else {
-            System.out.println("a < b\n");
-        }
-    }
-
-    public static void resultat() {
-        System.out.print("Задание 5: ");
-
-
-        boolean result = sumMethod(0, -1);
+        String result = a >= b ? "a >= b" : "a < b";
         System.out.println(result + "\n");
     }
 
-    public static boolean sumMethod(int a, int b) {
-        return a + b >= 10 && a + b <= 20;
+    public static void resultat(int a, int b) {
+        System.out.print("Задание 5: ");
+        boolean result = a + b >= 10 && a + b <= 20;
+        System.out.println(result + "\n");
     }
 
-
-    public static void nature() {
+    public static void nature(int a) {
         System.out.print("Задание 6: ");
-        numMethod(-4);
+        String result = a >= 0 ? "Число " + a + " положительное" : "Число " + a + " отрицательное";
+        System.out.println(result + "\n");
+
     }
 
-    public static void numMethod(int a) {
-        if (a >= 0) {
-            System.out.println("Число " + a + " положительное\n");
-        } else System.out.println("Число " + a + " отрицательное\n");
-    }
-
-    public static void beneficial() {
+    public static void beneficial(int a) {
         System.out.print("Задание 7: ");
-        boolean res1 = checkNum(-3);
+        boolean res1 = a <= 0;
         System.out.println(res1 + "\n");
-
-    }
-
-    public static boolean checkNum(int a) {
-        return a <= 0;
-    }
-
-
-    public static void line() {
-
-        str = "Good afternoon";
-        num = 2;
-        bass(str, num);
     }
 
     public static void bass(String str, int num) {
-        System.out.print("Задание 8: ");
-
+        System.out.println("Задание 8: ");
         for (int i = 0; i < num; i++) {
             System.out.println(str);
         }
     }
 
-
-    public static void leapYear() {
-        boolean res1 = isLeap(2024);
-        boolean res2 = isLeap(2023);
+    public static void leapYear(int year) {
         System.out.println("\nЗадание 9: ");
-        System.out.println(res1);
-        System.out.println(res2);
+        boolean result = (year % 4 == 0 || year % 100 == 0);
+        System.out.println(result);
 
     }
-
-    public static boolean isLeap(int year) {
-
-        if (year % 4 != 0 || year % 100 == 0) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-
 
     public static void metMassed() {
         System.out.println("\nЗадание 10: ");
@@ -129,13 +69,10 @@ public class Homework {
                 arr[i] = 0;
             } else if (arr[i] == 0) {
                 arr[i] = 1;
-
             }
             System.out.print(arr[i] + "\n");
         }
-
     }
-
 
     public static void massiveMethod() {
         System.out.println("Задание 11: ");
@@ -155,8 +92,6 @@ public class Homework {
             }
             System.out.println(arr[i]);
         }
-
-
     }
 
     public static void cubeMass() {
@@ -169,30 +104,21 @@ public class Homework {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.print("\n");
-
         }
         System.out.println("От такие пироги :) ");
     }
 
-
     public static int[] createArray(int len, int initialValue) {
+        System.out.println("Задание 14: ");
         int[] array = new int[len];
         for (int i = 0; i < len; i++) {
             array[i] = initialValue;
         }
-        return array;
-    }
-
-    public static void argumets() {
-        System.out.println("Задание 14: ");
-        int len = 12;
-        int initialValue = 7;
-        int[] newArray = createArray(len, initialValue);
-
-
-        for (int num : newArray) {
+        for (int num : array) {
             System.out.print(num + " ");
         }
+        return array;
     }
 }
+
 
